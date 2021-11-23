2e9b1a719b0876fc58b94f674eb556147783afcc
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-11-23 22:40:25 +0000

    First complete working draft

--- main.py
+++ main.py
@@ -103,4 +103,12 @@ print("")
 
 # Cast both integers to a string, concatenate, then back to a string again for love score
 love_score = int(str(true) + str(love))
-print(f"Your love score is {love_score}")
+
+if love_score < 10 or love_score > 90:
+  print(f"Your score is {love_score}, you go together like coke and mentos")
+elif love_score >=40 or love_score <= 50:
+  print(f"Your score is {love_score}, you are alright together")
+else:
+  print(f"Your love score is {love_score}")
+
+
