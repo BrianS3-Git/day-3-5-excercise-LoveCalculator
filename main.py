2f32a4a20b29d8c0636f8cac41e28add00dedd67
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-11-23 22:04:32 +0000

    First use of "and" in conditional statements, added if/else for singular/plural time/times. True section complete

--- main.py
+++ main.py
@@ -13,3 +13,43 @@ lower_case_name2 = name2.lower()
 print(lower_case_name1)
 print(lower_case_name2)
 
+true = 0
+# print(true)
+# Count for T in both names
+true_t_count = lower_case_name1.count("t") + lower_case_name2.count("t")
+if true_t_count < 2 and true_t_count > 0:
+  print(f"T occurs {true_t_count} time")
+else:
+  print(f"T occurs {true_t_count} times")
+
+true = true + true_t_count
+
+# Count for R in both names
+true_r_count = lower_case_name1.count("r") + lower_case_name2.count("r")
+if true_r_count < 2 and true_r_count > 0:
+  print(f"R occurs {true_t_count} time")
+else:
+  print(f"R occurs {true_t_count} times")
+
+true = true + true_r_count
+
+# Count for U in both names
+true_u_count = lower_case_name1.count("u") + lower_case_name2.count("u")
+if true_u_count < 2 and true_u_count > 0:
+  print(f"U occurs {true_u_count} time")
+else:
+  print(f"U occurs {true_u_count} times")
+
+true = true + true_u_count
+
+# Count for E in both names
+true_e_count = lower_case_name1.count("e") + lower_case_name2.count("e")
+if true_e_count < 2 and true_e_count > 0:
+  print(f"E occurs {true_e_count} time")
+else:
+  print(f"E occurs {true_e_count} times")
+
+true = true + true_e_count
+
+
+print(true)
